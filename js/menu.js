99213aa92639a637ab799d0129fbdb63bc7e39e8
Vinicius Sousa <viniciusxsousa@outlook.com>
2022-09-26 21:18:49 -0300

    resolvendo bug no menu

--- js/menu.js
+++ js/menu.js
@@ -13,6 +13,17 @@ function Menu(config){
     this.btn.removeAttribute('style');
     closeMenu();
 
+    if(this.maxWidth){
+        window.addEventListener('resize', e =>{
+            if(window.innerWidth > _this.maxWidth){
+                _this.nav.removeAttribute('style');
+                _opened = true;
+            } else if(!_this.nav.getAttribute('style')){
+                closeMenu();
+            }
+        })
+    }
+
     this.btn.addEventListener('click', operOrClose);
 
     function operOrClose(){
